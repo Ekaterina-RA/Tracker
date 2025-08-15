f5b1bb7364721e8d4851ba9b098431b903277848
Ekaterina <ekaterina.kuz@gmail.com>
2025-08-16 00:42:50 +0300

    small fixes

--- users/models.py
+++ users/models.py
@@ -3,4 +3,18 @@ from django.db import models
 
 
 class User(AbstractUser):
-    chat_id = models.CharField(max_length=100, blank=True, null=True)
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    chat_id = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="Telegram Chat ID"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
